946d196c2297d59550c7395402ff7b8615367bda
mahan85 <mahanbst85@gmail.com>
2020-09-12 02:18:24 -0700

    add file explorer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PIL import ImageGrab
+from tkinter import Tk
+from tkinter.filedialog import asksaveasfilename
+
 im = ImageGrab.grabclipboard()
-path = input('what path u want to save : ')
+Tk().withdraw()
+path = asksaveasfilename(defaultextension='.png', filetypes=[('images', '*.png')], title='where save screen shot')
+
 if im is not None:
     im.save(path, 'PNG')
 else:
