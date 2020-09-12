2efd2c071df059154e2ae4c9e86f2d4c012d91c3
mahan85 <mahanbst85@gmail.com>
2020-09-12 02:26:50 -0700

    add message box

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PIL import ImageGrab
 from tkinter import Tk
 from tkinter.filedialog import asksaveasfilename
-
+from tkinter import messagebox
 im = ImageGrab.grabclipboard()
 Tk().withdraw()
 path = asksaveasfilename(defaultextension='.png', filetypes=[('images', '*.png')], title='where save screen shot')
 
 if im is not None:
     im.save(path, 'PNG')
+    messagebox.showinfo("done!", "your image saved :)")
 else:
-    print('your clipboard doesnt have image')
+    messagebox.showerror("image not fount", "your clipboard doesnt have image")
