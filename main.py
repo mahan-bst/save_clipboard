d44d4a06ae3f91767bea92dc4e9c0355e52fead8
mahan85 <mahanbst85@gmail.com>
2020-09-13 00:54:00 -0700

    add it to right click

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from PIL import ImageGrab
 from tkinter import Tk
 from tkinter.filedialog import asksaveasfilename
 from tkinter import messagebox
+import sys
 im = ImageGrab.grabclipboard()
 Tk().withdraw()
 path = asksaveasfilename(defaultextension='.png',
                          filetypes=[('images', '*.png')],
                          title='where save screen shot',
-                         initialfile='screenShot.png')
+                         initialfile='screenShot.png',
+                         initialdir=sys.argv[1])
 
 if im is not None:
     im.save(path, 'PNG')
