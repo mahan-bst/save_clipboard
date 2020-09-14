0dc343280bf5248377c48ea0f4d58bfa254b845f
mahan85 <mahanbst85@gmail.com>
2020-09-14 08:00:41 -0700

    remove save as window when right click

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from tkinter import Tk
 from tkinter.filedialog import asksaveasfilename
 from tkinter import messagebox
 import sys
+import time
+import os
 im = ImageGrab.grabclipboard()
 Tk().withdraw()
-path = asksaveasfilename(defaultextension='.png',
-                         filetypes=[('images', '*.png')],
-                         title='where save screen shot',
-                         initialfile='screenShot.png',
-                         initialdir=sys.argv[1])
-
-if im is not None:
+if len(sys.argv) == 1:
+    path = asksaveasfilename(defaultextension='.png',
+                             filetypes=[('images', '*.png')],
+                             title='where save screen shot',
+                             initialfile='screenShot.png')
+else:
+    path = sys.argv[1]
+    path = os.path.join(path, str(time.time())+'.png')
+if im is not None and path != '':
     im.save(path, 'PNG')
     messagebox.showinfo("done!", "your image saved :)")
 elif path == '':
